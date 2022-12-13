3be22b256aab3ac62053f1b4a7536a5dd9d95818
beyrons <beyrons@gmail.com>
2022-12-13 15:21:17 +0500

    Init app. Сделана проверка относительности путей импорта

---
+++ tests/lib/rules/path-check.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-check"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("path-check", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
