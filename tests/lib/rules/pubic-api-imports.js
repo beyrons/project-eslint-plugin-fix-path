829e5a165f419cb65c51dcef4d1e4c3cd879f9f1
beyrons <beyrons@gmail.com>
2022-12-15 16:20:21 +0500

    #4. Public api imports. Имплементация для тестов.

--- tests/lib/rules/pubic-api-imports.js
+++ tests/lib/rules/pubic-api-imports.js
@@ -28,6 +28,24 @@ ruleTester.run("pubic-api-imports", rule, {
             errors: [],
             options: aliasOptions,
         },
+        {
+            filename: '/home/projects/web/src/entities/file.test.ts',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+            errors: [],
+            options: [{     // из "schema" pubic-api-imports.js
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        },
+        {
+            filename: '/home/projects/web/src/entities/StoreDecorator.tsx',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+            errors: [],
+            options: [{
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        }
     ],
 
     invalid: [
@@ -36,5 +54,23 @@ ruleTester.run("pubic-api-imports", rule, {
             errors: [{ message: "Абсолютный импорт разрешен только из public-api (index.ts)"}],
             options: aliasOptions,
         },
+        {
+            filename: '/home/projects/web/src/entities/StoreDecorator.tsx',         //  if (isImportNotFromPublicApi && !isTestingPublicApi) -- pubic-api-imports.js
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
+            errors: [{message: 'Абсолютный импорт разрешен только из public-api (index.ts)'}],
+            options: [{
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        },
+        {
+            filename: '/home/projects/web/src/entities/forbidden.ts',
+            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+            errors: [{message: 'Тестовые данные необходимо импортировать из "PublicApi/testing.ts"'}],
+            options: [{
+                alias: '@',
+                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+            }],
+        }
     ],
 });
